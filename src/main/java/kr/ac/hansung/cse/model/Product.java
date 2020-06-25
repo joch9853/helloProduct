89d8bf9d2a5cfb20a5946f5284a7ecfdb54bfde9
jongc <jongc@DESKTOP-R3BV7HJ>
2020-06-25 21:07:16 +0900

    init commit

---
+++ src/main/java/kr/ac/hansung/cse/model/Product.java
@@ -0,0 +1,42 @@
+package kr.ac.hansung.cse.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Product  implements Serializable{/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5492801535149319780L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
+	private int id;
+	
+	private String name;
+	private String category;
+	
+	private int price;
+	
+	private String manufacturer;
+	
+	@Column(name = "unit_in_stock")
+	private int unitInStock;
+	
+	private String description;
+}
